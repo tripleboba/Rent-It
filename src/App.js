7c8e579265ac080487165456dc9269b843c00ffe
Triple the Boba ! <coconuttt1010@gmail.com>
2022-03-19 18:33:48 -0700

    init components with react route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// components
+import Nav from "./components/Nav";
+import Home from './components/Home';
+import OnRenting from "./components/OnRenting";
+// stylesheet
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Rent It App
+      <Nav />
+
+      <Router>
+        <Routes>
+          <Route path="/" 
+            element={<Home/>}
+          />
+          <Route path="/renting"
+            element={<OnRenting />}
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/OnRenting.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function OnRenting() {
+  return (
+    <div>Me is OnRenting page</div>
+  )
+}
