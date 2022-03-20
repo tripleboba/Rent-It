efe0b44c210d5c0e842426dc817d0f61fb27c274
Triple the Boba ! <coconuttt1010@gmail.com>
2022-03-19 23:34:25 -0700

    basic page structure with css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,31 @@
 import { React } from 'react'
 import './Header.scss'
+import logo from '../styles/logo.png'
 
 export default function Header() {
   return (
 
-      <nav className="navbar is-warning" role="navigation" aria-label="main navigation">
+      <nav className="navbar" role="navigation" aria-label="main navigation">
         
-        <div classNameName="navbar-brand">
-          <div classNameName="navbar-item">
+        <div className="navbar-brand">
+          {/* <a  className="navbar-item" href="/">RENT IT</a> */}
+          {/* <div className="navbar-item"> */}
             <a  className="navbar-item"
-                href="/">RENT IT</a>
-          </div>
+                href="/">
+                {/* <img src={logo} alt="logo" width="auto" height="100%"/> */}
+                <strong>RENT IT</strong>
+            </a>
+          {/* </div> */}
         </div>
         
         <div className="navbar-end">
-          <div className="navbar-item">
+          {/* <div className="navbar-item"> */}
             <a  className="navbar-item"
-                href="/renting">On Renting (0 item) </a>
+                href="/renting"> On Renting (0 item) </a>
             <a  className="navbar-item"
-                href="/rented">Rented History </a>
+                href="/rented"> Rented History </a>
             <div className="navbar-item"> Signup/Login </div>
-          </div>
+          {/* </div> */}
         </div>
 
       </nav>
