a8a5344f984d8908067950ca5ac77fde6c103313
Jolin Zhang <jolinhlzhang@gmail.com>
2022-03-21 14:29:12 -0600

    Delete db directory

--- db/seeds/items.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO items (id, title, image, price, description, address, cart_id)
-VALUES (1, 'Toyota RAV4', 'https://assets.gcs.ehi.com/content/enterprise_cros/data/vehicle/bookingCountries/CA/SUVS/IFAR.doi.768.high.imageSmallThreeQuarterNodePath.png/1618262943657.png', 30, 'Intermediate SUV', '2000 Airport Rd NE, Calgary, AB T2E 6W5', 1),
-(2, 'Ford Edge', 'https://assets.gcs.ehi.com/content/enterprise_cros/data/vehicle/bookingCountries/CA/SUVS/SFAR.doi.768.high.imageSmallThreeQuarterNodePath.png/1551905658879.png', 35, 'Standard SUV', '2000 Airport Rd NE, Calgary, AB T2E 6W5', 1),
-(3, 'Dodge Grand Caravan', 'https://assets.gcs.ehi.com/content/enterprise_cros/data/vehicle/bookingCountries/CA/VANS/MVAR.doi.768.high.imageSmallThreeQuarterNodePath.png/1574880395246.png', 40,'7 PASSENGER MINIVAN', '405 Spray Ave, Banff, AB T1L 1J4', 1),
-(4, 'Chevy Colorado', 'https://assets.gcs.ehi.com/content/enterprise_cros/data/vehicle/bookingCountries/CA/TRUCKS/SPAR.doi.768.high.imageSmallThreeQuarterNodePath.png/1535121223938.png', 42, 'Small pickup truck ', '405 Spray Ave, Banff, AB T1L 1J4', 1);
-
-
